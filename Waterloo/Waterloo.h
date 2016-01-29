21a26eadee50dc09a774593a93110d65468dadb4
Jon Cotton <jonathan.cotton@sky.uk>
2016-01-29 19:11:22 +0000

    Initial Commit

---
+++ Waterloo/Waterloo.h
@@ -0,0 +1,19 @@
+//
+//  Waterloo.h
+//  Waterloo
+//
+//  Created by Cotton, Jonathan (Mobile Developer) on 29/01/2016.
+//  Copyright © 2016 Jon Cotton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Waterloo.
+FOUNDATION_EXPORT double WaterlooVersionNumber;
+
+//! Project version string for Waterloo.
+FOUNDATION_EXPORT const unsigned char WaterlooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Waterloo/PublicHeader.h>
+
+
